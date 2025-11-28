f7d7db7a4563c05e4c30043ecc336d689fde20e8
LobbyTech <1187755698@qq.com>
2025-11-28 21:59:38 +0800

    1.21.1

--- src/main/java/io/github/thebusybiscuit/slimefunluckyblocks/surprises/lucky/LuckyPickaxeSurprise.java
+++ src/main/java/io/github/thebusybiscuit/slimefunluckyblocks/surprises/lucky/LuckyPickaxeSurprise.java
@@ -18,9 +18,9 @@ public final class LuckyPickaxeSurprise implements Surprise {
 	
 	public LuckyPickaxeSurprise() {
 		pickaxe = new CustomItemStack(Material.GOLDEN_PICKAXE, "&e&l幸运镐子");
-		pickaxe.addUnsafeEnchantment(Enchantment.DIG_SPEED, 10);
-		pickaxe.addUnsafeEnchantment(Enchantment.LOOT_BONUS_BLOCKS, 10);
-		pickaxe.addUnsafeEnchantment(Enchantment.DURABILITY, 10);
+		pickaxe.addUnsafeEnchantment(Enchantment.EFFICIENCY, 10);
+		pickaxe.addUnsafeEnchantment(Enchantment.LOOTING, 10);
+		pickaxe.addUnsafeEnchantment(Enchantment.UNBREAKING, 10);
 	}
 	
 	@Override
--- src/main/java/io/github/thebusybiscuit/slimefunluckyblocks/surprises/unlucky/WalshrusSurprise.java
+++ src/main/java/io/github/thebusybiscuit/slimefunluckyblocks/surprises/unlucky/WalshrusSurprise.java
@@ -24,9 +24,9 @@ public final class WalshrusSurprise implements Surprise {
 
     public WalshrusSurprise() {
         sword = new CustomItemStack(Material.GOLDEN_SWORD, "&e&l幸运剑");
-        sword.addUnsafeEnchantment(Enchantment.DAMAGE_ALL, 10);
-        sword.addUnsafeEnchantment(Enchantment.LOOT_BONUS_MOBS, 10);
-        sword.addUnsafeEnchantment(Enchantment.DURABILITY, 10);
+        sword.addUnsafeEnchantment(Enchantment.SHARPNESS, 10);
+        sword.addUnsafeEnchantment(Enchantment.LOOTING, 10);
+        sword.addUnsafeEnchantment(Enchantment.UNBREAKING, 10);
         sword.addUnsafeEnchantment(Enchantment.FIRE_ASPECT, 5);
     }
 
